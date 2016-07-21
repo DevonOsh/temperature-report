b332832d3f89b86df2a2ebbaf724cdaa6a287ff8
Devon O'Shaughnessy <devono@ulfoods.com>
2016-07-21 15:58:38 +0000

    Changed the index js file to what it's supposed to be

--- Temperature Report/js/index.js
+++ Temperature Report/js/index.js
@@ -1,50 +1,50 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+(function(temp){
+    var application,
+    	app = temp.app = temp.app || {};
+    
+    document.addEventListener("deviceready", function() {
         navigator.splashscreen.hide();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        application = new kendo.mobile.Application(
+            document.body, {
+                initial: "components/login/loginView.html",
+                transition: "",
+                layout: "",
+                skin: "flat"
+            }
+ 		);
+    });
+    
+    var JSDOSettings = {
+        "serviceURI": "http://10.0.1.239:8810/Temp_Report",
+        "catalogURIs": "http://10.0.1.239:8810/Temp_Report/static/Temp_ReportService.json",
+        "authenticationModel": "anonymous"
+    };
+    
+    app.JSDOSession = new progress.data.Session();
+    app.JSDOSession.login(JSDOSettings.serviceURI);
+    app.JSDOSession.addCatalog(JSDOSettings.catalogURIs);
+    
+    app.locJSDO = new progress.data.JSDO({
+        name: "Temp_Loc"
+    });
+    
+    app.loginJSDO = new progress.data.JSDO({
+        name: "Mobile_User"
+    });
+    
+    app.reportJSDO = new progress.data.JSDO({
+        name: "Temp_Report",
+        autoFill: false
+    })
+    
+    app.sendReport= function (reportModel) {
+            app.JSDOSession.addCatalog(JSDOSettings.catalogURIs);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+            app.reportJSDO.create(reportModel);
+            app.reportJSDO.saveChanges();
+        },
+    
+    app.goToTempInput = function() {application.navigate("components/tempInput/tempInputView.html","slide");}
+    app.goToScan = function() {application.navigate("components/barcodeScan/barcodeScanView.html","slide");}
+})(window);
 
-        console.log('Received Event: ' + id);
-    }
-};
